13a3971c42b8d4058b29a4078bd1799397594776
NikitaNikumbh <npnikumbh99@gmail.com>
2022-07-06 16:44:41 +0530

    Updates

---
+++ precache-manifest.8b60e2b53e1f63b1b218cac98d92c9d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f6fcad66e3a54eb24f",
+    "url": "/ExpenseApp/static/css/main.454456fa.chunk.css"
+  },
+  {
+    "revision": "e7f6fcad66e3a54eb24f",
+    "url": "/ExpenseApp/static/js/main.e7f6fcad.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/ExpenseApp/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "60ea6223dcd624d3cc0b",
+    "url": "/ExpenseApp/static/js/2.60ea6223.chunk.js"
+  },
+  {
+    "revision": "9154f8c19f31e96dfdf4",
+    "url": "/ExpenseApp/static/js/runtime~main.9154f8c1.js"
+  },
+  {
+    "revision": "829cc2597f67ffb74c1347ae191baa72",
+    "url": "/ExpenseApp/static/media/img1.829cc259.webp"
+  },
+  {
+    "revision": "51f918a0c69fed3cb3edc7f6e08398bf",
+    "url": "/ExpenseApp/index.html"
+  }
+];
